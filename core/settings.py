4d26694db919ccd14c197a083e6cbc0833a21eef
Suriya Prakash <suriyag1999@gmail.com>
2024-10-27 06:29:17 +0000

    dev postgres container configured

--- core/settings.py
+++ core/settings.py
@@ -69,7 +69,7 @@ DATABASES = {
         "USER": config("DB_USER"),
         "PASSWORD": config("DB_PASSWORD"),
         "HOST": config("DB_HOST"),
-        "PORT": config("DB_POPRT"),
+        "PORT": config("DB_PORT"),
     }
 }
 
