d01a7f075d7e3fc85eecd2731f8061a368af889b
Suriya Prakash <suriyag1999@gmail.com>
2024-10-28 07:34:01 +0000

    import and export package installed and configured

--- api/admin.py
+++ api/admin.py
@@ -4,6 +4,8 @@ from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
 from django.contrib.auth.models import User, Group
 from unfold.admin import ModelAdmin
 from unfold.forms import UserChangeForm, UserCreationForm, AdminPasswordChangeForm
+from import_export.admin import ImportExportModelAdmin
+from unfold.contrib.import_export.forms import ExportForm, ImportForm, SelectableFieldsExportForm
 
 from .models import Currency, Product, Category, Supplier, ProductSupplier, ProductImage, Warehouse, Stock
 
@@ -21,17 +23,58 @@ class UserAdmin(BaseUserAdmin, ModelAdmin):
 class GroupAdmin(BaseGroupAdmin, ModelAdmin):
     pass
 
+class CurrencyAdmin(ModelAdmin, ImportExportModelAdmin):
+    import_form_class = ImportForm
+    export_form_class = ExportForm
+    export_form_class = SelectableFieldsExportForm
+
+class ProductAdmin(ModelAdmin, ImportExportModelAdmin):
+    import_form_class = ImportForm
+    export_form_class = ExportForm
+    export_form_class = SelectableFieldsExportForm
+
+class CategoryAdmin(ModelAdmin, ImportExportModelAdmin):
+    import_form_class = ImportForm
+    export_form_class = ExportForm
+    export_form_class = SelectableFieldsExportForm
+
+class SupplierAdmin(ModelAdmin, ImportExportModelAdmin):
+    import_form_class = ImportForm
+    export_form_class = ExportForm
+    export_form_class = SelectableFieldsExportForm
+
+class ProductSupplierAdmin(ModelAdmin, ImportExportModelAdmin):
+    import_form_class = ImportForm
+    export_form_class = ExportForm
+    export_form_class = SelectableFieldsExportForm
+
+class ProductImageAdmin(ModelAdmin, ImportExportModelAdmin):
+    import_form_class = ImportForm
+    export_form_class = ExportForm
+    export_form_class = SelectableFieldsExportForm
+
+class WarehouseAdmin(ModelAdmin, ImportExportModelAdmin):
+    import_form_class = ImportForm
+    export_form_class = ExportForm
+    export_form_class = SelectableFieldsExportForm
+
+class StockAdmin(ModelAdmin, ImportExportModelAdmin):
+    import_form_class = ImportForm
+    export_form_class = ExportForm
+    export_form_class = SelectableFieldsExportForm
+    
+admin.site.register(Currency, CurrencyAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Supplier, SupplierAdmin)
+admin.site.register(ProductSupplier, ProductSupplierAdmin)
+admin.site.register(ProductImage, ProductImageAdmin)
+admin.site.register(Warehouse, WarehouseAdmin)
+admin.site.register(Stock, StockAdmin)
+
+
 
 # class UserTokenAdmin(admin.ModelAdmin):
 #     list_display = ('user','token','expiration_date', 'used', 'expired')
 #     list_filter = ("used", "expired", "expiration_date")
 #     search_fields = ['user']
-    
-admin.site.register(Currency, ModelAdmin)
-admin.site.register(Product, ModelAdmin)
-admin.site.register(Category, ModelAdmin)
-admin.site.register(Supplier, ModelAdmin)
-admin.site.register(ProductSupplier, ModelAdmin)
-admin.site.register(ProductImage, ModelAdmin)
-admin.site.register(Warehouse, ModelAdmin)
-admin.site.register(Stock, ModelAdmin)
